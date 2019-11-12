591562e354f262790a9947317428733a0b30e0a5
Evan Jones <evanjones@fastmail.net>
2019-11-12 17:29:07 -0500

    added API component

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default {
+  search: function(query, diet, intolerances) {
+    return axios({
+      method: "GET",
+      url:
+        "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "a59f0571fbmsh4ee374c9e48291fp12d0bdjsna458c493e660"
+      },
+      params: {
+        query: query,
+        diet: diet,
+        intolerances: intolerances,
+        addRecipeInformation: "true",
+        limitLicense: "false",
+        offset: "0",
+        number: "10"
+      }
+    });
+  }
+};
