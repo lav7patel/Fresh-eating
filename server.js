e613a0a29f5409409772755dd1a4596401a5fefe
Lav Patel <lavpatel9507@gmail.com>
2019-11-12 20:22:28 -0500

    Add mongo database, just plug and play

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongojs = require("mongojs");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//const db = mongojs( need a database URL here)
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
