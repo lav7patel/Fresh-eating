97f9d473af4889c15532ba2a60a2ad66e16b1db1
Rachel Puder <rcompuder@gmail.com>
2019-11-14 14:01:40 -0500

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,18 +3,8 @@ import React, { useState, useEffect } from "react";
 function Navbar(props) {
   return (
   <div class="navbar">
-  <a href="#home">Home</a>
-  <a href="#news">News</a>
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown 
-      <i class="fa fa-caret-down"></i>
-    </button>
-      <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
+  <a href="#home">Search</a>
+  <a href="#news">Favorites</a>
 </div>)
   //<div> Navbar </div>;
 
