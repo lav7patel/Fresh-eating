798ca04b3bfd9c25e11d315fffeb9c3a33253f84
Evan Jones <evanjones@fastmail.net>
2019-11-14 20:21:13 -0500

    basic get and post routes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const Recipe = require("./models/recipe.js");
+
 //const db = mongojs( need a database URL here)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipes", {
+  useNewUrlParser: true
+});
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +20,25 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.post("/api/recipe", ({ body }, res) => {
+  Recipe.create(body)
+    .then(dbRecipe => {
+      res.json(dbRecipe);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/recipe", (req, res) => {
+  Recipe.find({})
+    .then(dbRecipe => {
+      res.json(dbRecipe);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
