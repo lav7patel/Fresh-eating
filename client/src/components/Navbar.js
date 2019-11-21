a1e51371dbcce76e5c7fb2a9abbacfa2a662fb66
Nataliej <natalieu00@gmail.com>
2019-11-21 16:30:48 -0500

    working on more CSS

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,18 @@
-import React, { useState, useEffect } from "react";
+  
+import React from "react";
 
 function Navbar(props) {
-  return <div> Navbar </div>;
+  return (
+    <div className="topnav">
+      <a href="/favorites">
+        <i class="fa fa-star"></i>Favorites
+      </a>
+      <a href="/login">Login</a>
+      <a href="/">Search</a>
+    </div>
+  );
+
+  //<div> Navbar </div>;
 }
 
 export default Navbar;
