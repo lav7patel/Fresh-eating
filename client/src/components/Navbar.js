303c09394409e4a7709629e8b2733f20329907e1
Rachel Puder <rcompuder@gmail.com>
2019-11-21 21:02:06 -0500

    updated css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
       <a href="/favorites">
         <i class="fa fa-star"></i>Favorites
       </a>
-      <a href="/login">login</a>
+      <a href="/login">Login</a>
       <a href="/">Search</a>
     </div>
   );
