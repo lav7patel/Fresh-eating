6d2ef3de3bd3f5bfb24ba2acc684b11e44dc5217
Nataliej <natalieu00@gmail.com>
2019-11-23 15:33:39 -0500

    back to old nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,15 +3,15 @@ import React from "react";
 function Navbar(props) {
   return (
     <div className="topnav">
-      <a href="/favorites">
-        <i class="fa fa-star"></i>Favorites
-      </a>
-      <a href="/login">login</a>
-      <a href="/">Search</a>
+    <a href="/favorites">
+      <i class="fa fa-star"></i>Favorites
+    </a>
+    <a href="/login">Login</a>
+    <a href="/">Search</a>
     </div>
   );
+  }
 
-  //<div> Navbar </div>;
-}
 
 export default Navbar;
+  
