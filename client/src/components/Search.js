f7b025e275d502df7790e3b91d69948c22af54af
Rachel Puder <rcompuder@gmail.com>
2019-11-26 12:08:33 -0500

    background and jumboron edit

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -178,6 +178,7 @@ class Search extends Component {
         <div className="searchbox">
           <div className="jumbotron jumbotron-fluid">
            <div className="container">
+             <br></br>
               <h1 className="display-4">Fresh-Eating</h1>
               <div classNAme= "searchbardislplay">
             <div className="searchpart">
