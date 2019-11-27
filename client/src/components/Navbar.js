a745bbbd4df23fc5b0afd8c64790532f1ec59543
Rachel Puder <rcompuder@gmail.com>
2019-11-26 19:38:01 -0500

    comment edits in search

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Navbar(props) {
   return (
-    
-<div className="topnav">
+     /*This is the navbar with 3 elements*/ 
+  <div className="topnav">
       <a href="/favorites"><i class="fa fa-heart"></i>Favorites</a>
       <a href="/login">Login</a>
       <a href="/">Search</a>
